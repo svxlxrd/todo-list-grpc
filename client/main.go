e7ef427800b3a46f4ddd26700490f297965cd77a
Михаил <mishakhegai@gmail.com>
2026-07-15 00:03:52 +0300

    feat(client): add client logic

--- client/main.go
+++ client/main.go
@@ -1,1 +1,34 @@
-package client
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/svxlxrd/todo-list-grpc/gen/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewTodoServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	// вызовы
+	res, err := c.CreateTask(ctx, &pb.CreateTaskRequest{
+		Title: "Купить молоко",
+	})
+	if err != nil {
+		log.Fatalf("could not create task: %v", err)
+	}
+
+	log.Printf("Response from server: Task ID: %s, Title: %s", res.Id, res.Title)
+}
